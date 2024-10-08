db9b2f906498bf8927030efaeb94a9f7bbd14f57
Raj <rpal778866@gmail.com>
2024-10-08 21:48:53 +0530

    chore: small fix

--- app/ItemByCategory/page.tsx
+++ app/ItemByCategory/page.tsx
@@ -2,44 +2,49 @@
 import SectionHeader from "@/components/section-header";
 import { Container, Grid } from "@mui/material";
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import ProductCard from "../components/product-card";
 
 const CategoryPage = () => {
   const searchParams = useSearchParams();
-  const text = searchParams.get("text"); // Extract the 'text' query parameter
+  const text = searchParams.get("text");
 
   return (
-    <Container maxWidth="xl" sx={{ my: 4 }}>
-      <SectionHeader sectionTitle={text ?? ""} sectionName="Item By Category" />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Container maxWidth="xl" sx={{ my: 4 }}>
+        <SectionHeader
+          sectionTitle={text ?? ""}
+          sectionName="Item By Category"
+        />
 
-      <Grid container spacing={3}>
-        <Grid item xs={3}>
-          <ProductCard />
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={3}>
+            <ProductCard />
+          </Grid>
         </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={3}>
-          <ProductCard />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </Suspense>
   );
 };
 
