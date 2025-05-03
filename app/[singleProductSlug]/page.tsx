5d824c4bbbac3af02e2f1c537d6960433cb6ff76
Raj <rpal778866@gmail.com>
2025-05-03 21:43:20 +0530

    feat: auth fix

--- app/[singleProductSlug]/page.tsx
+++ app/[singleProductSlug]/page.tsx
@@ -11,7 +11,6 @@ import ProductRatings from "./components/product-ratings";
 import { Marked } from "marked";
 import ImageSlideShow from "./components/image-slide-show";
 import { Metadata } from "next";
-import { parseEntities } from "parse-entities";
 import { truncateTextWithEllipsis } from "@/utils";
 import { DOMAIN_NAME } from "@/constants";
 
@@ -46,22 +45,31 @@ async function getProductById(
   }
 }
 
+function decodeHTMLEntities(str: string): string {
+  return str
+    .replace(/&nbsp;/g, " ")
+    .replace(/&amp;/g, "&")
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'");
+}
+
 export async function generateMetadata({
   params,
 }: SingleProductPageProps): Promise<Metadata> {
   const id = params.singleProductSlug;
-
   const product = await getProductById(id);
 
   if (!product) return {};
 
   const parsedDescription = await marked.parse(product.description);
 
-  const plainTextDescripion = truncateTextWithEllipsis(
-    parseEntities(
+  const plainTextDescription = truncateTextWithEllipsis(
+    decodeHTMLEntities(
       parsedDescription
-        .replace(/<\/?[^>]+(>|$)/g, "") // Strip HTML tags
-        .replace(/\n/g, " ") // Replace line breaks with spaces
+        .replace(/<\/?[^>]+(>|$)/g, "")
+        .replace(/\n/g, " ")
         .trim(),
     ),
     150,
@@ -71,7 +79,7 @@ export async function generateMetadata({
     title: product.name,
     openGraph: {
       title: product.name,
-      description: plainTextDescripion,
+      description: plainTextDescription,
       url: DOMAIN_NAME,
       images: product.images.map((image) => ({
         url: image,
@@ -96,12 +104,14 @@ const SingleProductPage = async ({ params }: SingleProductPageProps) => {
   }
 
   if (error) {
-    return <Typography>Something went wrong !!!</Typography>;
+    return <Typography sx={{ m: 4 }}>Something went wrong: {error}</Typography>;
   }
 
   if (!product) {
-    return <Typography>No ProductType found !!!</Typography>;
+    return <Typography sx={{ m: 4 }}>Product not found.</Typography>;
   }
+
+  const htmlDescription = await marked.parse(product.description);
 
   return (
     <Container
@@ -110,7 +120,7 @@ const SingleProductPage = async ({ params }: SingleProductPageProps) => {
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
-        marginBottom: 10,
+        mb: 10,
       }}
     >
       <Grid
@@ -123,21 +133,22 @@ const SingleProductPage = async ({ params }: SingleProductPageProps) => {
         <Grid item xs={1}>
           <ImageSlideShow images={product.images} />
         </Grid>
+
         <Grid item xs={1} lg={1.5}>
-          <Typography sx={{ fontWeight: "bold" }} fontSize={{ xs: 20, sm: 24 }}>
+          <Typography fontWeight="bold" fontSize={{ xs: 20, sm: 24 }}>
             {product.name}
           </Typography>
+
           <Box
-            display={"flex"}
-            justifyContent={"flex-start"}
+            display="flex"
+            justifyContent="flex-start"
             mt={2}
-            alignItems={"center"}
+            alignItems="center"
           >
             <ProductRatings rating={product.averageRating} />
-            <Typography
-              fontSize={{ xs: 12, sm: 16 }}
-              sx={{ ml: 1 }}
-            >{`${product.numOfReviews} reviews`}</Typography>
+            <Typography fontSize={{ xs: 12, sm: 16 }} sx={{ ml: 1 }}>
+              {`${product.numOfReviews} reviews`}
+            </Typography>
 
             <Divider
               orientation="vertical"
@@ -154,22 +165,18 @@ const SingleProductPage = async ({ params }: SingleProductPageProps) => {
               {product.inventory === 0 ? "Out Of Stock" : "In Stock"}
             </Typography>
           </Box>
+
           <Typography mt={2} fontSize={{ xs: "20px", sm: "24px" }} variant="h6">
             {formatCurrency(product.price)}
           </Typography>
+
           <Box
-            sx={{
-              "& ul": {
-                marginY: 0,
-              },
-              "& h4": {
-                marginY: 1,
-              },
-            }}
             mt={2}
-            dangerouslySetInnerHTML={{
-              __html: await marked.parse(product.description),
+            sx={{
+              "& ul": { my: 0 },
+              "& h4": { my: 1 },
             }}
+            dangerouslySetInnerHTML={{ __html: htmlDescription }}
           />
 
           <Divider sx={{ mt: { xs: 2, md: 4 } }} />
@@ -178,7 +185,6 @@ const SingleProductPage = async ({ params }: SingleProductPageProps) => {
       </Grid>
 
       <Divider sx={{ mt: { xs: 4, md: 8 } }} />
-
       <AddReviewForm productId={product._id} />
       <ReviewList reviews={product.reviews} />
     </Container>
